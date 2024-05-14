c32d08830ab8f15db82f3581634435322b94f37b
Jason Park <jpark@labbase.org>
2024-05-14 09:48:15 -0700

    4

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -68,14 +68,15 @@ const ContactPage = () => {
           className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
         >
           <span>Dear Jason Park,</span>
-          <textarea
-            rows={6}
-            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
-            name="user_message"
-          />
           <span>My name is:</span>
           <input
             name="user_name"
+            type="text"
+            className="bg-transparent border-b-2 border-b-black outline-none"
+          />
+          <span>Context:</span>
+          <input
+            name="user_message"
             type="text"
             className="bg-transparent border-b-2 border-b-black outline-none"
           />
