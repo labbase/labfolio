85b8325c0367ac67ec704e31fc04e0aefe0a86ba
Jason Park <jpark@labbase.org>
2024-06-05 15:24:45 -0700

    v0.0.8

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -90,22 +90,22 @@ const Navbar = () => {
       </div>
       {/* SOCIAL */}
       <div className="hidden md:flex gap-4 w-1/3">
-        <Link href="/">
+        <Link href="https://github.com/labbase">
           <Image src="/github.png" alt="" width={24} height={24} />
         </Link>
-        <Link href="/">
+        {/* <Link href="/">
           <Image src="/dribbble.png" alt="" width={24} height={24} />
         </Link>
         <Link href="/">
           <Image src="/instagram.png" alt="" width={24} height={24} />
-        </Link>
-        <Link href="/">
+        </Link> */}
+        {/* <Link href="/">
           <Image src="/facebook.png" alt="" width={24} height={24} />
         </Link>
         <Link href="/">
           <Image src="/pinterest.png" alt="" width={24} height={24} />
-        </Link>
-        <Link href="/">
+        </Link> */}
+        <Link href="https://www.linkedin.com/in/jason-changsin-park-03882616b">
           <Image src="/linkedin.png" alt="" width={24} height={24} />
         </Link>
       </div>
